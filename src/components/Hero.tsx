d075b27c385c31cbc3e7174b9b40b7c51763c906
Tung Duong Ta <duongtatung@gmail.com>
2024-12-22 18:20:02 +1100

    quick fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -166,7 +166,7 @@ export default function Hero() {
     >
       {isLoading && (
         <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50 ">
-          <div className="">
+          <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
