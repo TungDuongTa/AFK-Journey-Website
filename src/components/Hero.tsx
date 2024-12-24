00c290846c7cc550968d3ba2bef79954822df255
Tung Duong Ta <duongtatung@gmail.com>
2024-12-24 19:09:27 +1100

    fixing bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -237,34 +237,10 @@ export default function Hero() {
         <div className="absolute left-0 top-0 z-30 h-full w-full bg-black opacity-30 pointer-events-none"></div>
         {/* Preload all videos */}
         <div className="invisible">
-          <video
-            src={getVideoSrc(1)}
-            loop
-            muted
-            preload="auto"
-            onLoadedData={handleVideoLoad}
-          />
-          <video
-            src={getVideoSrc(2)}
-            loop
-            muted
-            preload="auto"
-            onLoadedData={handleVideoLoad}
-          />
-          <video
-            src={getVideoSrc(3)}
-            loop
-            muted
-            preload="auto"
-            onLoadedData={handleVideoLoad}
-          />
-          <video
-            src={getVideoSrc(4)}
-            loop
-            muted
-            preload="auto"
-            onLoadedData={handleVideoLoad}
-          />
+          <video src={getVideoSrc(1)} muted preload="auto" />
+          <video src={getVideoSrc(2)} muted preload="auto" />
+          <video src={getVideoSrc(3)} muted preload="auto" />
+          <video src={getVideoSrc(4)} muted preload="auto" />
         </div>
         <div>
           {/* mivi video */}
