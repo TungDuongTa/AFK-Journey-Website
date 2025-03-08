c9e63dc952ef5ec13d51d98801f5a375b9be8826
Tung Duong Ta <duongtatung@gmail.com>
2025-03-08 14:06:52 +1100

    fix scroll

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const location = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     // Scroll to the top on route change
     window.scrollTo(0, 0);
   }, [location]);
